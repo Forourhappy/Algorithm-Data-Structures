09a5d8503672ec80c64832b95cfb60c8c4b5fee6
Forourhappy <foroh1504@gmail.com>
2023-01-17 00:32:34 +0900

    Level0 1문제

--- Programers/Level0/외계어 사전.js
+++ Programers/Level0/외계어 사전.js
@@ -1,4 +1,14 @@
+const spell = ['p', 'o', 's'];
+const dic = ['sod', 'eocd', 'qixm', 'adio', 'soo'];
 function solution(spell, dic) {
-	const spellSet = new Set(...spell);
-	spellSet.add();
+	// 스펠을 체크하기 위해서는 배열 속의 문자열을 확인해야 함.
+	// 배열 속의 문자열이 아닌 문자열만 돈다면 O(n^2)을 피할 수 있음.
+	const dicStr = dic.join(' ');
+	let spellSet = new Set(spell);
+	for (i of dicStr) {
+		if (spellSet.has(i)) spellSet.delete(i);
+		if (i === ' ') spellSet = new Set(spell);
+		if (spellSet.size === 0) return 1;
+	}
+	return 2;
 }
