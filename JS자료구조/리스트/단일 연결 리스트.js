9583aa9e5e15828b8f6f18926347dc272d1a008d
Forourhappy <foroh1504@gmail.com>
2023-02-10 09:36:44 +0900

    SinglyLinkedList 초기 설정

---
+++ JS자료구조/리스트/단일 연결 리스트.js
@@ -0,0 +1,16 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    // 다음 노드를 가리킨다
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = null;
+  }
+  
+}
