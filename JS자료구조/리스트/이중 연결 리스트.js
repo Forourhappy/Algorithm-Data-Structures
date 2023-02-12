ebbc108e9a33fe3521d74e3bf652addc3e707623
Forourhappy <foroh1504@gmail.com>
2023-02-12 22:33:56 +0900

    DoublyLinkedList shift, unshift 메소드 작성

--- JS자료구조/리스트/이중 연결 리스트.js
+++ JS자료구조/리스트/이중 연결 리스트.js
@@ -30,13 +30,13 @@ class DoublyLinkedList {
 
 	pop() {
 		if (this.length === 0) return undefined;
+		const poppedNode = this.tail;
     // 노드가 1개일 때는 head와 tail 모두 null이 되도록 설정.
 		if (this.length === 1) {
 			this.head = null;
 			this.tail = null;
 		} else {
       // pop할 노드를 저장
-			const poppedNode = this.tail;
       // tail을 이전 노드로 옮기기
 			this.tail = poppedNode.prev;
       // pop할 노드와의 연결 끊기
@@ -44,6 +44,35 @@ class DoublyLinkedList {
 			this.tail.next = null;
 		}
 		this.length--;
+		return poppedNode;
+	}
+
+	shift() {
+		if(this.length === 0) return undefined;
+		const shiftedNode = this.head;
+		if(this.length === 1) {
+			this.head = null;
+			this.tail = null;
+		} else {
+			this.head = shiftedNode.next;
+			this.head.prev = null;
+			shiftedNode.next = null;
+		}
+		this.length--;
+		return shiftedNode;
+	}
+
+	unshift(val) {
+		const newNode = new Node(val);
+		if(this.length === 0) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			newNode.next = this.head;
+			this.head.prev = newNode;
+			this.head = newNode;
+		}
+		this.length++;
 		return this;
 	}
 }
