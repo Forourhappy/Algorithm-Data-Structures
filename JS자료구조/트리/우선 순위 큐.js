fbd3cc252c4eb2a4dcd91a015e124529edeaf1d8
Forourhappy <foroh1504@gmail.com>
2023-02-22 23:55:12 +0900

    PriorityQueue 작성, Binary Heap Big O 추가
    Min Binary Heap 이용

--- JS자료구조/트리/우선 순위 큐.js
+++ JS자료구조/트리/우선 순위 큐.js
@@ -1,34 +1,37 @@
-// 우선순위 큐에 사용
 
-// Max Binary Heap
-// 부모 노드는 자식 노드보다 값이 큼
-// 값은 왼쪽부터 채워짐
-// 왼쪽과 오른쪽 형제 사이에 순서는 없음
-// 부모의 인덱스는 Math.floor((index-1)/2)
+// 우선순위 큐
+// Min Binary Heap 사용
+// 각 요소가 그에 해당하는 우선순위를 가짐
 
-// Min Binary Heap
-// 두 자식의 값은 부모 노드의 값보다 큼
-// 자식의 인덱스는 index * 2 + 1, index * 2 + 2
+class Node {
+    constructor(val, priority) {
+        this.val = val;
+        this.priority = priority;
+    }
+}
 
-class MaxBinaryHeap {
+class PriorityQueue {
     constructor() {
         this.values = [];
     }
 
-    insert(val) {
-        this.values.push(val);
+    enqueue(val, priority) {
+        let newNode = new Node(val, priority);
+        this.values.push(newNode);
         this.bubbleUp();
-        return this;
     }
 
+    // 우선 순위가 높은 수(priority가 낮은 수)를 위로 옮기는 작업
     bubbleUp() {
         // 삽입한 값의 인덱스
         let index = this.values.length - 1;
+        const element = this.values[index];
         while (index > 0) {
             // 삽입한 값의 부모 인덱스
             let parentIdx = Math.floor((index - 1) / 2);
-            // 삽입한 값이 부모보다 작다면 종료
-            if (this.values[parentIdx] >= this.values[index]) break;
+            let parent = this.values[parentIdx];
+            // 삽입한 값이 부모보다 크다면 종료
+            if (parent.priority <= element.priority) break;
             // 아니라면 서로 자리 바꾸기
             [this.values[index], this.values[parentIdx]] = [this.values[parentIdx], this.values[index]];
             // 삽입한 값의 인덱스 위치를 업데이트
@@ -36,22 +39,22 @@ class MaxBinaryHeap {
         }
     }
 
-    // 최대 값 추출
-    extractMax() {
-        // 최대 값은 루트 노드
-        const max = this.values[0];
-        // 맨 끝 값은 나중에 max와 바꿔줄 것
+    // 우선 순위가 높은 수(priority가 낮은 수) dequeue
+    dequeue() {
+        // 우선 순위가 높은 값은 루트 노드
+        const min = this.values[0];
+        // 맨 끝 값은 나중에 min과 바꿔줄 것
         const end = this.values.pop();
-        // values가 1개일 때, max === end edgeCase 체크
+        // values가 1개일 때, min === end edgeCase 체크
         if (this.values.length > 0) {
             // end를 맨 앞으로 옮기기
             this.values[0] = end;
             this.bubbleDown();
         }
-        return max
+        return min;
     }
 
-    // 작은 수를 아래로 옮기는 작업
+    // 우선 순위가 낮은 수(priority가 높은 수)를 아래로 옮기는 작업
     bubbleDown() {
         // 현재 노드 위치
         let index = 0;
@@ -68,21 +71,21 @@ class MaxBinaryHeap {
             // 왼쪽 자식 노드가 배열의 인덱스 범위 내인지 체크
             if (leftChildIdx < length) {
                 leftChild = this.values[leftChildIdx];
-                // 현재 노드보다 왼쪽 자식 노드의 값이 더 크다면
+                // 현재 노드의 우선 순위보다 왼쪽 자식 노드의 우선 순위가 더 높다면
                 // 현재 노드와 왼쪽 자식 노드를 바꾸기
                 // (실제로 바뀌는 것은 오른쪽 체크도 끝난 뒤)
-                if (leftChild > element) swap = leftChildIdx;
+                if (leftChild.priority < element.priority) swap = leftChildIdx;
             }
 
             if (rightChildIdx < length) {
                 rightChild = this.values[rightChildIdx];
                 if (
                     // 현재 노드가 왼쪽 자식 노드와 바뀌지 않고,
-                    // 오른쪽 자식 노드보다 값이 더 작거나,
-                    (swap === null && rightChild > element) ||
+                    // 오른쪽 자식 노드의 우선 순위가 더 높거나,
+                    (swap === null && rightChild.priority < element.priority) ||
                     // 현재 노드가 왼쪽 자식 노드와 바뀌고,
-                    // 바뀐 값보다 오른쪽 자식 노드의 값이 더 크면
-                    (swap !== null && rightChild > leftChild)
+                    // 바뀐 값의 우선 순위보다 오른쪽 자식 노드의 우선 순위가 더 높다면
+                    (swap !== null && rightChild.priority < leftChild.priority)
                 ) {
                     // 왼쪽이 아니라 오른쪽 자식 노드와 바꾸기
                     swap = rightChildIdx;
@@ -97,8 +100,3 @@ class MaxBinaryHeap {
         }
     }
 }
-
-// 시간 복잡도
-// Insertion - O(log n)
-// Removal - O(log n)
-// Search - O(N)
